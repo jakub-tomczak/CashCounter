f4e7fe545d1253a0e9f11b5c9a729be1ffa7116e
jakub-tomczak <jakub.tomczak@live.com>
2017-11-20 14:42:27 +0100

    Add hough circles and remove small contours.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from __future__ import division
-from pylab import *
+
 from skimage import io, filters, feature, transform, draw,exposure
 import skimage.morphology as mp
 import skimage.measure as measure
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as graph
-from skimage.color import rgb2gray
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.measure import regionprops
+from skimage.color import rgb2gray, gray2rgb
 from skimage import img_as_float, img_as_ubyte
-from IPython.display import display
+from skimage.draw import circle_perimeter
 
+from pylab import *
+import matplotlib.patches as mpatches
 import numpy as np
 from ipykernel.pylab.backend_inline import flush_figures
-from skimage.measure import regionprops
 
 '''
 http://scikit-image.org/docs/dev/user_guide/tutorial_segmentation.html
@@ -19,6 +20,23 @@ https://github.com/astroluj/VC_OpenCV_Coin_Detection
 http://www.emgu.com/wiki/files/1.4.0.0/html/0ac8f298-48bc-3eee-88b7-d2deed2a285d.htm
 http://scikit-image.org/docs/dev/auto_examples/features_detection/plot_template.html
 '''
+
+class coin:
+    file_extension = "jpg"
+    in_dir = "data/"
+    out_dir = "out/"
+    def __init__(self, img):
+        self.img_oryg = img
+        self.img_contoured = []
+        self.segments = []
+        self.img_size = 0
+
+    def read_image(self):
+        pass
+    def find_contours(self, f):
+        img_contoured = f(self.img_oryg)
+
+
 
 def hsv2rgb(h, s, v, alpha = False):
     import math
@@ -128,34 +146,24 @@ def detector_canny(image):
 
     #drawPlot(image)
 
-def on_change(img,perc=0):
-    MIN = np.percentile(img, perc)
-    MAX = np.percentile(img, 100-perc)
-    #Percentyl – kwantyl rzędu k/100, gdzie k=1, … , 99.
-    #Intuicyjnie mówiąc, percentyl jest wielkością, poniżej której padają wartości zadanego procentu próbek.
+def region_detector(img,t):
 
-    norm = (img - MIN) / (MAX - MIN)
-    norm[norm[:,:] > 1] = 1
-    norm[norm[:,:] < 0] = 0
+    binary = (img > t) * 255
+    binary = np.uint8(binary)
+    binary = mp.dilation(binary)
 
-    figure(figsize=(15,5))
-    subplot(1,2,1); plt.imshow(norm, cmap='gray')
-    subplot(1,2,2); plot_hist(norm)
-    flush_figures()
+    binary = mp.label(binary)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.imshow(binary)
 
-def find(img,level=0.5):
-    contours = measure.find_contours(img, level)
-    fig, ax = plt.subplots()
-    ax.imshow(img, interpolation='nearest', cmap=plt.cm.gray)
+    for region in regionprops(binary):
+        if region.area >= 1500:
+            minr, minc, maxr, maxc = region.bbox
+            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                          fill=False, edgecolor='red', linewidth=2)
+            ax.add_patch(rect)
 
-
-    for n, contour in enumerate(contours):
-        if len(contour) > 100:
-            ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-
-    ax.axis('image')
-    ax.set_xticks([])
-    ax.set_yticks([])
+    ax.set_axis_off()
     plt.show()
 
 def plot_hist(img):
@@ -181,71 +189,146 @@ def getMax(image):
     maxV = max(np.max(image, axis=1))
     return maxV
 
+path = ""
+extension = "'"
 def readImage(name):
-    return  io.imread("data/old/{img}.png".format(img = name))
+    #filename = "%03d" % name
+    file = "{path}/{name}.{extension}".format(path = path, name = name, extension = extension)
+    print("Trying to open", file)
+    return  io.imread(file)
 
 def canny(img, name):
-    img2 = feature.canny(img, 4)
+    img2 = mp.dilation(feature.canny(img, 4))
     #drawPlot(img2, img, name)
     return img2
 
 def circle_detector(x, y, num):
     xc = np.mean(x)
     yc = np.mean(y)
-    r = (x-xc)**2 + (y-yc)**2 #kiepski pomysł - zakłada środek w (0,0)
+    r = (x-xc)**2 + (y-yc)**2 #try to figure out radius of the circle with the middle of the xc and yc
+
+    #circle estimator
     #plt.Circle((xc,yc), 20, color='k')
     #xy = measure.CircleModel().predict_xy(y, params=(2, 3, 4))
 
     #r = measure.CircleModel()
     #return r.estimate(np.array( tuple(zip(x,y)) ))
 
-    data = [num, np.mean(r), np.std(r), 100*np.std(r)/np.mean(r), "% ", np.percentile(r, 75) - np.percentile(r, 25), xc, yc]
-    return (" ".join(map(str, data)), np.std(r)/np.mean(r))
-    #print(num, np.mean(r), np.std(r), 100*np.std(r)/np.mean(r), "% ", np.percentile(r, 75) - np.percentile(r, 25))
+    #data = [num, np.mean(r), np.std(r), 100.jpg*np.std(r)/np.mean(r), "% ", np.percentile(r, 75) - np.percentile(r, 25), xc, yc]
+    #return (" ".join(map(str, data)), np.std(r)/np.mean(r))
+    return ("%.2f" % (100 * np.std(r) / np.mean(r)), ( np.std(r) / np.mean(r)))
+
+    #verbose version
+    #print(num, np.mean(r), np.std(r), 100.jpg*np.std(r)/np.mean(r), "% ", np.percentile(r, 75) - np.percentile(r, 25))
 
 #uses find contours and percentile threshold
-def findContours(img):
+
+def findContours(img, number):
+    img_size = len(img)*len(img[0])
     contours = measure.find_contours(img, .8, fully_connected='high')
     sizes = [len(contour) for contour in contours]
-    perc = 10
+    print(np.mean(sizes), np.percentile(sizes, 75) - np.percentile(sizes, 25))
+    perc = 25
     perMin = np.percentile(sizes, perc)
     perMax = np.percentile(sizes, 100 - perc)
-    cleaned = [contour for contour in contours if ( len(contour) > perMin and len(contour) < perMax )]
+    #remove using percentiles
+    #cleaned = [contour for contour in contours if ( len(contour) > perMin and len(contour) < perMax)]
+    #remove using possible shape surface
+    cleaned = [contour for contour in contours if ( (np.max(contour[:,1])-np.min(contour[:,1])) * (np.max(contour[:,0])-np.min(contour[:,0])) > img_size*0.01 )]
+
     # Display the image and plot all contours found
     fig, ax = plt.subplots()
-
     ax.imshow(img, interpolation='nearest', cmap=plt.cm.gray)
     iter = 0
-    for n, contour in enumerate(contours):
+    for n, contour in enumerate(cleaned):
+        if iter > 0:
+            break
+        fig, ax = plt.subplots()
+        contour = flip_pixels(contour, (int(np.mean(contour[:,0])), int(np.mean(contour[:,1]))))
+
         text, percent = circle_detector(contour[:, 1], contour[:, 0], iter)
-        if percent > .75:
-            continue
+
         ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
 
-        patch = plt.Circle((int(np.mean(contour[:,1])), int(np.mean(contour[:,0]))), .2, color='blue')
+        patch = plt.Circle((int(np.mean(contour[:,1])), int(np.mean(contour[:,0]))), 2, color='r')
         ax.add_patch(patch)
-        #ax.text(int(np.min(contour[:,1])), int(np.min(contour[:,0])), text , fontsize=8)
-        #plt.savefig("testR/{}.png".format(iter))
+        ax.text(int(np.min(contour[:,1])), int(np.min(contour[:,0])), text ,color = 'r' , fontsize=8)
+        #rect = plt.Rectangle()
+        #plt.show()
+        #flush_figures()
+        #plt.savefig("out/{}_detected.png".format(iter))
 
 
         iter += 1
-    #return 0
     ax.axis('image')
     ax.set_xticks([])
     ax.set_yticks([])
     plt.show()
+    #plt.savefig("out/{}_detected.png".format(number))
+
+
+#img - image after canny
+def hough_circles_detector(img, oryg):
+    hough_radii = np.arange(30, 140, 20)
+    hough_res = hough_circle(img, hough_radii)
+
+    # Select the most prominent 5 circles
+    accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,
+                                           total_num_peaks=3)
+
+    # Draw them
+    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
+    iter = 1
+    for center_y, center_x, radius in zip(cy, cx, radii):
+        iter+=1
+        circy, circx = circle_perimeter(center_y, center_x, radius)
+        if len(oryg[circy, circx][0]) == 4:
+            oryg[circy, circx] = [220, 20, 20, 255]
+        else:
+            oryg[circy, circx] = [220, 20, 20]
+
+
+
+    print("Found ", iter, " circles")
+    ax.imshow(oryg)
+    plt.show()
+
+def flip_pixels(contours, mean_point):
+    new_contours = np.zeros((2*len(contours), 2))
+    iter = 0
+    for (x,y) in contours:
+        new_contours[iter] = [x, y]
+        iter+=1
+
+    m_x = mean_point[0]
+    m_y = mean_point[1]
+
+    for (x,y) in contours:
+        to_append = [m_x - (x-m_x), m_y - (y - m_y)]
+        new_contours[iter] = to_append
+        iter+=1
+    return new_contours
 
 def main():
-    img = readImage(2)
-    img = rgb2gray(img)
+    global path
+    path = "data/old"
+    global extension
+    extension = "png"
+    #img = readImage(2)
+    #img = rgb2gray(img)
 #    find(img)
     #[canny(rgb2gray(readImage(i)), "{}.png".format(i)) for i in range(1,5)]
+    imgs_oryg = [readImage(i) for i in range(2, 3)]
+    imgs = [canny(rgb2gray(imgs_oryg[i]), "out/old_{}.png".format(i)) for i in range(0, len(imgs_oryg))]
+    #customized circles detector
+    [findContours(imgs[i], i+1) for i in range(0, len(imgs))]
 
-    imgs = [canny(rgb2gray(readImage(i)), "{}.png".format(i)) for i in range(1, 5)]
-    [findContours(image) for image in imgs]
-    #on_change(img, 5)
-    #processOne(3)
-    #[processOne(i) for i in range(1,5)]
+    #region detector with threshold
+    #[region_detector(img, .7) for img in imgs]
+
+    #hough circles
+    #[hough_circles_detector(img, oryg) for img, oryg in zip(imgs, imgs_oryg)]
+
 
 if __name__ == "__main__":
     main()
